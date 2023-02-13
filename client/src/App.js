8f23549e2f4ec4b8abfb1c31ffb0642351f00d32
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-02-13 20:35:44 +0000

    events data bases added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-
+import Events from "./components/eventSection/EventsPage";
 import About from "./pages/About";
 import Home from "./pages/Home";
 
@@ -7,7 +8,10 @@ const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
+		<Route path="/events" element={<Events />} />
 	</Routes>
 );
+
+
 
 export default App;
--- server/api.js
+++ server/api.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import logger from "./utils/logger";
-
+import db from "./db";
 const router = Router();
 
 router.get("/", (_, res) => {
@@ -10,3 +10,26 @@ router.get("/", (_, res) => {
 });
 
 export default router;
+
+
+
+// GET "/"
+router.get("/events", (req, res) => {
+	db.query("SELECT * FROM events")
+		.then((result) => res.json(result.rows))
+		.catch((error) => {
+			console.error(error);
+			res.status(200).json(error);
+		});
+});
+
+router.get("/events/:id", (req, res) => {
+	const eventId = req.params.id;
+	db
+		.query("SELECT * FROM events WHERE id=$1", [eventId])
+		.then((result) => res.json(result.rows))
+		.catch((error) => {
+			console.error(error);
+			res.status(200).json(error);
+		});
+});
