d91e80b629679431dce58381d0273bd0f9e8b51d
Dawit Abraha <akdawit1@gmail.com>
2023-02-13 23:24:47 +0000

    Removing a route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
-import Booking from "./components/bookingSection/Booking";
+// import Booking from "./components/bookingSection/Booking";
 import Home from "./pages/Home";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
-		<Route path="/booking" element={<Booking />} />
+
+		{/* <Route path="/booking" element={<Booking />} /> */}
 	</Routes>
 );
 
