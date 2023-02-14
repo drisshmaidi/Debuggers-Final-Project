fb13c4300493a1b02fff795481c6fc3cb00e5775
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-02-14 20:15:50 +0000

    css updated and date modified

--- client/src/components/eventSection/EventsPage.js
+++ client/src/components/eventSection/EventsPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./appEvent.css";
+// import event from "../eventSection/event.jpg";
 
 function EventsPage() {
 	const [events, setEvents] = useState([]);
@@ -18,19 +19,39 @@ function EventsPage() {
 		fetchEvent();
 	}, []);
 
-	return events.map((event) => (
-		<section className="event" key={event.id}>
-			<div class="row">
-				<div className="titleEvent">
-					<h1>{event.title}</h1>
-					<img width="500" height="300" src={event.img} title="Event Title" />
-					<h2>{event.location}</h2>
-					<h2>{event.date}</h2>
-					<p className = "descriptionEvent">{event.description}</p>
+const formatDate = (dateString) => {
+	const date = new Date(dateString);
+	return date.toLocaleDateString();
+};
+
+	return (
+		<>
+			<div className="wholePage">
+				<h1 className="allEvents">All Events</h1>
+				{/* <img src={event} alt="" width="1800px" height="700px" /> */}
+
+				<div className="eventsContainer">
+					{events.map((event) => (
+						<section className="event" key={event.id}>
+							<div className="eventContent">
+								<h1>{event.title}</h1>
+								<img
+									width="500"
+									height="300"
+									src={event.img}
+									title="Event Title"
+								/>
+								<h2>{event.location}</h2>
+								<h2>{formatDate(event.date)}</h2>
+								<p className="descriptionEvent">{event.description}</p>
+							</div>
+						</section>
+					))}
 				</div>
 			</div>
-		</section>
-	));
+		</>
+	);
+	
 }
 
 export default EventsPage;
