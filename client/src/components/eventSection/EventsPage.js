fc139737680163f71895272303915009b39bf0fa
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-02-14 21:43:09 +0000

    more events added

--- client/src/components/eventSection/EventsPage.js
+++ client/src/components/eventSection/EventsPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./appEvent.css";
-// import event from "../eventSection/event.jpg";
+import event from "../eventSection/event.png";
 
 function EventsPage() {
 	const [events, setEvents] = useState([]);
@@ -10,8 +10,11 @@ function EventsPage() {
 		fetch(`/api/events`)
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data);
-				setEvents(data);
+				const currentDate = new Date();
+				const futureEvents = data.filter(
+					(event) => new Date(event.date) >= currentDate
+				);
+				setEvents(futureEvents);
 			});
 	};
 
@@ -19,22 +22,23 @@ function EventsPage() {
 		fetchEvent();
 	}, []);
 
-const formatDate = (dateString) => {
-	const date = new Date(dateString);
-	return date.toLocaleDateString();
-};
+	const formatDate = (dateString) => {
+		const date = new Date(dateString);
+		return date.toLocaleDateString();
+	};
 
 	return (
 		<>
-			<div className="wholePage">
-				<h1 className="allEvents">All Events</h1>
-				{/* <img src={event} alt="" width="1800px" height="700px" /> */}
-
+			<h1 className="allEvents">All Events</h1>
+			<img src={event} alt="" width="1750px" height="600px" class="center" />
+			{events.length === 0 ? (
+				<p>There are no events planned.</p>
+			) : (
 				<div className="eventsContainer">
-					{events.map((event) => (
+					{events.map((event, index) => (
 						<section className="event" key={event.id}>
 							<div className="eventContent">
-								<h1>{event.title}</h1>
+								<h1 className="eventTitle">{event.title}</h1>
 								<img
 									width="500"
 									height="300"
@@ -44,14 +48,18 @@ const formatDate = (dateString) => {
 								<h2>{event.location}</h2>
 								<h2>{formatDate(event.date)}</h2>
 								<p className="descriptionEvent">{event.description}</p>
+								<p>
+									<a href={event.url} target="_blank">
+										See details
+									</a>
+								</p>
 							</div>
 						</section>
 					))}
 				</div>
-			</div>
+			)}
 		</>
 	);
-	
 }
 
 export default EventsPage;
