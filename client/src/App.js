1b5a708f61deb72e582edc02b1e2035eece73f16
Dawit Abraha <akdawit1@gmail.com>
2023-02-15 19:38:22 +0000

    booking page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Events from "./components/eventSection/EventsPage";
 import About from "./pages/About";
-// import Booking from "./components/bookingSection/Booking";
+import Booking from "./components/bookingSection/Booking";
 import Home from "./pages/Home";
 
-const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-		<Route path="/events" element={<Events />} />
-{/* <Route path="/booking" element={<Booking />} /> */}
- 
-	</Routes>
-);
+const App = () => {
+	const [eventId, setEventId] = useState();
 
-
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about/this/site" element={<About />} />
+			<Route path="/events" element={<Events setEventId={setEventId} />} />
+			<Route path="/booking" element={<Booking eventId={eventId} />} />
+		</Routes>
+	);
+};
 
 export default App;
