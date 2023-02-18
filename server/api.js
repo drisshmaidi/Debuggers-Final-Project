a9c51aaa199cf82ad70995e27b1465b783c9861f
OmidOmar <a.omidomari@gmail.com>
2023-02-18 11:17:40 +0000

    front end updated

--- server/api.js
+++ server/api.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import  { Router } from "express";
 import db from "./db";
 const fileUpload = require("express-fileupload");
 
@@ -14,12 +14,16 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 
-router.post("/checkUserTpe",(req,res)=>{
+router.post("/checkUserType",(req,res)=>{
+
+
 	const userId = req.body.userId;
-	logger.debug(userId);
 	db.query(
-		"SELECT ut.type FROM users u JOIN users_type ut ON u.type_id = ut.id WHERE u.id = $1",[userId]
-	).then((result) => res.json(result.rows));
+		"SELECT ut.type FROM users u JOIN users_type ut ON u.type_id = ut.id WHERE u.id = $1",[userId])
+	.then((result) => res.status(200).json(result.rows))
+	.catch((err)=> {
+		logger.debug(err);
+	});
 });
 
 //insert new event into database
@@ -27,8 +31,14 @@ router.post("/checkUserTpe",(req,res)=>{
 router.post("/addNewEvent",(req,res)=>{
 
 try{
-	saveEventPictures(req.files,res);
-	res.status(200).json({ message:"Event Saved Successfully" });
+	const event = req.body;
+	const [title, desc, startDate,time, endDate, email, mobile, location,userId] = Object.keys(event).map((key) => event[key]);
+	const image = saveEventPictures(req.files, res);
+logger.debug(endDate&&"null");
+	db.query("INSERT INTO events (title, description, image, start_date, end_date, time, location, email, mobile, user_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)"
+	,[title,desc,image,startDate,endDate?endDate:null,time,location,email,mobile?mobile:null,userId])
+	.then(() => res.status(200).json({ message:"Event Saved Successfully" }))
+	.catch((err)=>logger.debug(err));
 
 } catch(err){
 	logger.debug(err);
@@ -40,20 +50,23 @@ const saveEventPictures = (file, res) => {
 	if (!file || Object.keys(file).length === 0) {
 		return res.status(400).send("No files were uploaded.");
 	}
-	// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+	// The name of the eventPic is used to retrieve the uploaded file
 	const eventPic = file.eventPic;
-logger.debug(Math.floor(Math.random() * 10000000000));
+	const fileName =
+		Math.floor(Math.random() * 10000000000) +
+		"." +
+		eventPic.name.split(".").pop();
+		logger.debug(__dirname+"../");
 	const uploadPath =
 		__dirname +
-		"/Event-Pictures/" +
-		(Math.floor(Math.random() * 10000000000) +
-		10000000000) +"." + eventPic.name.split(".").pop();
-	// Use the mv() method to place the file somewhere on your server
+		"/Event-Pictures/" +fileName;
+	// Use the mv() method to place the file somewhere on the server
 	eventPic.mv(uploadPath, function (err) {
 		if (err) {
 			return res.status(500).send(err);
 		}
 	});
+	return fileName;
 };
 
 export default router;
