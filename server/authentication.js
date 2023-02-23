c3be9d7dafb1d2f243e8b1d1f93561ac255f724b
OmidOmar <a.omidomari@gmail.com>
2023-02-23 21:26:47 +0000

    authentication upated

--- server/authentication.js
+++ server/authentication.js
@@ -3,10 +3,7 @@ import jwt from "jsonwebtoken";
 //Authorization Middleware
 const authorization = (req, res, next) => {
 	//get token
-
-
-	const token = req.headers.authorization;
-	req.body.testMsg = "Hello from Authorization";
+	const token = req.headers.authentication;
 	try {
 		//if token exist
 		if (token) {
@@ -15,13 +12,18 @@ const authorization = (req, res, next) => {
 				token.split(" ")[1],
 				process.env.JWT_SECRET
 			);
-            req.body.authentication ={ userId:userId,username:username };
+			req.body.authentication = {
+				username: username,
+                userId: userId,
+				status: 200,
+				authMsg: "Token is valid",
+			};
 		} else {
-			req.body.authMsg = "Invalid Token";
+			req.body.authentication = { staus: 498, authMsg: "Token not found" };
 		}
 	} catch (err) {
 		//return error if token or secret key is invalid
-		req.body.authMsg = "Invalid signature";
+		req.body.authentication = { staus: 498, authMsg: "Invalid token" };
 	}
 	next();
 };
